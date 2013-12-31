ff08d931b9a32aa7ba36426d39f53c11c0921d82
sdlong <sdlong.jeng@gmail.com>
2013-12-31 11:42:31 +0800

    update ch-2

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,6 @@
 class Group < ActiveRecord::Base
 
+	has_many :posts
+
 	validates :title, :presence => true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Groupme::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root :to => "groups#index"
 
-  resources :groups
+  resources :groups do
+  	resources :posts
+  end
 end
